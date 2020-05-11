23d74ddc7c36f65b40e1508502dbdac85383a7d1
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-05-11 20:04:13 -0300

    Analog Clock [ok]

---
+++ script.js
@@ -0,0 +1,20 @@
+const PONTEIROHORA = document.querySelector("#hour");
+const PONTEIROMINUTO = document.querySelector("#minute");
+const PONTEIROSEGUNDO = document.querySelector("#second");
+
+function executarRelogio() {
+  let data = new Date();
+  let hr = data.getHours();
+  let min = data.getMinutes();
+  let seg = data.getSeconds();
+
+  let posicaoHr = (hr * 360) / 12 + (min * (360 / 60)) / 12;
+  let posicaoMin = (min * 360) / 60 + (seg * (360 / 60)) / 60;
+  let posicaoSeg = (seg * 360) / 60;
+
+  PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+  PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+  PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+}
+
+var intervalo = setInterval(executarRelogio, 1000);
